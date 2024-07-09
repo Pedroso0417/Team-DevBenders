b8a383e720baf05b5e7bdc4d2dee546b4d8d92bb
Pedroso0417/goit-js-hw-8 <136574676+Pedroso0417@users.noreply.github.com>
2024-07-10 03:58:05 +0800

    commit code-2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import styles from './App.module.css';
-import SwipeModal from './Swipe/swipeModal';
+import SwipeModal from './Swipe/swipeModal'; // Ensure this path is correct
 
 // Importing images
 import image1 from '../components/devPhoto/images/image-1.png';
@@ -9,57 +9,71 @@ import image3 from '../components/devPhoto/images/image-3.jpeg';
 import image4 from '../components/devPhoto/images/image-4.png';
 import image5 from '../components/devPhoto/images/image-5.png';
 import image6 from '../components/devPhoto/images/image-6.jpeg';
-import image7 from '../components/devPhoto/images/image-7.png';
+import image7 from '../components/devPhoto/images/image-7.jpeg';
 import image8 from '../components/devPhoto/images/image-8.png';
+import image9 from '../components/devPhoto/images/image-9.png';
+import image10 from '../components/devPhoto/images/image-10.png';
 
 const teamMembers = [
   {
     id: 1,
-    name: 'Member 1',
+    name: 'Bill Mico Malazarte',
     role: 'Team-Leader',
     imgSrc: image1,
   },
   {
     id: 2,
-    name: 'Member 2',
+    name: 'Alexander Dinoy Jr.',
     role: 'Scrum-master',
     imgSrc: image2,
   },
   {
     id: 3,
-    name: 'Member 3',
+    name: 'Cyfred Odarve',
     role: 'Scrum-master',
     imgSrc: image3,
   },
   {
     id: 4,
-    name: 'Member 4',
+    name: 'Crisanto Pedroso',
     role: 'Scrum-master',
     imgSrc: image4,
   },
   {
     id: 5,
-    name: 'Member 5',
+    name: 'Franci Reyes Giordani',
     role: 'Scrum-master',
     imgSrc: image5,
   },
   {
     id: 6,
-    name: 'Member 6',
+    name: 'Judielyn Pagente',
     role: 'Scrum-master',
     imgSrc: image6,
   },
   {
     id: 7,
-    name: 'Member 7',
+    name: 'Jovelyn Molos',
     role: 'Scrum-master',
     imgSrc: image7,
   },
   {
     id: 8,
-    name: 'Member 8',
+    name: 'Jed Alston Fallorina',
     role: 'Scrum-master',
     imgSrc: image8,
+  },
+  {
+    id: 9,
+    name: 'Jan Dilbert Ampuan',
+    role: 'Scrum-master',
+    imgSrc: image9,
+  },
+  {
+    id: 10,
+    name: 'Percival Aceron',
+    role: 'Scrum-master',
+    imgSrc: image10,
   },
 ];
 
--- src/components/Swipe/swipeModal.js
+++ src/components/Swipe/swipeModal.js
@@ -1,26 +1,25 @@
 import React from 'react';
-import css from './swipeModal.module.css';
+import styles from './swipeModal.module.css';
 
-const swipeModal = ({ onClose, teamMembers }) => {
+function SwipeModal({ onClose, teamMembers }) {
   return (
-    <div className={css.modal}>
-      <div className={css.modalContent}>
-        <span className={css.close} onClick={onClose}>
-          &times;
-        </span>
-        <h2>Team Members</h2>
-        {teamMembers.map(member => (
-          <div key={member.id} className={css.teamMember}>
-            <img src={member.imgSrc} alt={member.name} />
-            <div className={css.memberInfo}>
-              <h3>{member.name}</h3>
-              <p>Role: {member.role}</p>
+    <div className={styles.modal}>
+      <div className={styles.modalContent}>
+        <button className={styles.closeButton} onClick={onClose}>
+          Close
+        </button>
+        <div className={styles.gridContainer}>
+          {teamMembers.map(member => (
+            <div key={member.id} className={styles.gridItem}>
+              <img src={member.imgSrc} alt={member.name} />
+              <p className={styles.memberName}>{member.name}</p>
+              <p className={styles.membeName}>{member.role}</p>
             </div>
-          </div>
-        ))}
+          ))}
+        </div>
       </div>
     </div>
   );
-};
+}
 
-export default swipeModal;
+export default SwipeModal;
